4df6949bec169c931e3b81d2eb9051874b343e17
Edwin Heij <e.heij@konnectit.nl>
2021-10-08 07:26:02 +0200

    Added Laravel service provider

--- src/BinanceServiceProvider.php
+++ src/BinanceServiceProvider.php
@@ -8,8 +8,8 @@ class BinanceServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->bind(API::class, function() {
-            return new API(
+        $this->app->bind(Api::class, function() {
+            return new Api(
                 config('services.binance.key'),
                 config('services.binance.secret')
             );
